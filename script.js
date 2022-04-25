44a25b990187a62fd5c136f176ec847c3a2c1885
alex-alexandrade <alexandrade1516@hotmail.com>
2022-04-25 11:49:05 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function cliqueMe(){
+    document.querySelector(".teste").innerHTML="Clicou"
+}
+
+function passaMe(){
+    document.querySelector(".teste").innerHTML="Passou"
+}
+function saiuMe(){
+    document.querySelector(".teste").innerHTML="Ja Passou"
+}
